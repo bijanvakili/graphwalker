193e3e9a83a82a5e141db2a8c5bbff9d9bfaf104
bijanvakili <bijanvakili@users.noreply.github.com>
2017-07-01 19:30:39 -0700

    Upgrade to backbone.typeahead 1.1.4

--- app/globals.js
+++ app/globals.js
@@ -19,17 +19,7 @@ SvgStyles = require('./lib/svg.styles');
 window.$ = $;
 window._ = _;
 window.Backbone = Backbone;
-
-/**
- * Workaround until backbone.typeahead acceps PR #30
- * https://github.com/mojotech/backbone.typeahead/pull/30
- */
-
-require('backbone.typeahead/dist/backbone.typeahead');
-
-// TODO replace the above line with the following once the PR is merged and released
-// require('backbone.typeahead');
-
+require('backbone.typeahead');
 window.P = P;
 window.SVG = SVG;
 window.SvgStyles = SvgStyles;
